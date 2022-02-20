db19a05de685f7cf8f03099848b6e9dcc502ab32
gravity-gurl <oliviarahee.lee@gmail.com>
2022-02-20 13:31:34 -0800

    Initial commit

---
+++ first.js
@@ -0,0 +1,32 @@
+const ugogurl = document.getElementById('ugogurl')
+
+const ugobabe = document.createElement('div')
+ugobabe.className = 'conversation'
+ugobabe.textContent = 'ugobabe-ugogurl! I want food!!!!!!!! give me food!!!!'
+ugogurl.appendChild(ugobabe)
+
+const ugoboy = document.createElement('div')
+ugoboy.className = 'conversation'
+ugoboy.textContent = 'ugoboy-ugogirl!!! Play with meeeee!!! WeehWeeeh!!'
+ugogurl.appendChild(ugoboy)
+
+const ugomom = document.createElement('div')
+ugomom.className = 'conversation'
+ugomom.textContent = 'ugomom-ugogirl!!! Babysit ur lil\' sis!!'
+ugogurl.appendChild(ugomom)
+
+const ugodaddy = document.createElement('div')
+ugodaddy.className = 'conversation'
+ugodaddy.textContent = 'ugodaddy-ugogirl!!! Give me the coke in the fridge!!'
+ugogurl.appendChild(ugodaddy)
+
+const ugoneighbor = document.createElement('div')
+ugoneighbor.className = 'conversation'
+ugoneighbor.textContent = 'ugoneighbor-ugogurl!!! Babysit my children!! I will pay u one cent!!'
+ugogurl.appendChild(ugoneighbor)
+
+const ugopolice = document.createElement('div')
+ugopolice.className = 'warning'
+ugopolice.textContent = 'ugopolice-ugogurl you are under arrest!!'
+ugogurl.appendChild(ugopolice)
+
